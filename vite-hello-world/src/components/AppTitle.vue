1291517cb878130020fa27434a9f1d9f52258adc
MicheleGresta <malachia_1997@hotmail.it>
2023-08-21 18:18:43 +0200

    AppTitle component

---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+
+<script>
+    export default {
+
+    }
+</script>
+
+
+
+<template>
+         <h1 class="titleColor">
+            La mia prima app con Vite
+        </h1>
+</template>
+
+<style>
+
+    .titleColor{
+        color: greenyellow;
+        font-size: 3rem;
+    }
+</style>
+
